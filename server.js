3f2a34de88e36390b7b585e979f3d9947d41e33e
jvanosdol <j.vanosdol99@yahoo.com>
2022-09-16 16:09:46 -0700

    messing around with erq res objects

--- server.js
+++ server.js
@@ -18,6 +18,10 @@ app.use(express.json());
 
 app.use(express.static('public'));
 
+function writeToDB(note) {
+    fs.appendFileSync('./db/db.json', JSON.stringify(note));
+}
+
 app.get('/', (req, res) =>
   res.sendFile(path.join(__dirname, './public/index.html'))
 );
@@ -27,15 +31,48 @@ app.get('/notes', (req, res) => {
     res.sendFile(path.join(__dirname, './public/notes.html'))
     // Log our request to the terminal
     console.info(`${req.method} request received to get reviews`);
-
 }
   
  
 );
 
-app.post('/notes', (req, res) =>
-  res.sendFile(path.join(__dirname, './public/notes.html'))
-);
+app.get('/api/notes', (req, res) => {
+    console.log('/api/notes-get');
+    //console.log(res.json(db));
+    console.log(res);
+});
+
+app.post('/notes', (req, res) => {
+    //res.sendFile(path.join(__dirname, './public/notes.html'));
+    let newNote = req.body;
+    console.log(req)
+    newNote.id = uuid.v1();
+    db.push(newNote);
+    writeToDB(db);
+    
+
+    const newNotes = {
+        title: req.body.title,
+        text: req.body.text,
+        note_id: uuid.v1()
+      };
+
+    // req.body.title
+    // req.body.text
+
+      const reviewString = JSON.stringify(newNotes);
+
+db.push(reviewString)
+
+      fs.writeFileSync(`./db/db.json`, reviewString, (err) =>
+      err
+        ? console.error(err)
+        : console.log(
+            `Review for user has been written to JSON file`
+          )
+    );
+
+});
 
 app.get('*', (req, res) => {
     res.sendFile(path.join(__dirname, './public/index.html'));
