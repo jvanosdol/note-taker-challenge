f479f9e9f500e6caf894beb97ebf7b2a39451ab1
jvanosdol <j.vanosdol99@yahoo.com>
2022-09-17 18:04:46 -0700

    tweaked some small stuff

--- server.js
+++ server.js
@@ -23,7 +23,8 @@ function writeToDB(note) {
 }
 
 app.get('/', (req, res) =>
-  res.sendFile(path.join(__dirname, './public/index.html'))
+  //res.sendFile(path.join(__dirname, './public/index.html'))
+  res.writeFileSync(path.join(__dirname, './public/index.html'))
 );
 
 app.get('/notes', (req, res) => {
