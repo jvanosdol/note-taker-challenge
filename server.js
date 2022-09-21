f0fcf41a509b33d465f440147a83ba9846e1fe89
jvanosdol <j.vanosdol99@yahoo.com>
2022-09-21 14:01:59 -0700

    worked on post and delete routes with tutor

--- server.js
+++ server.js
@@ -20,15 +20,36 @@ app.use(express.static('public'));
 
 let db = require('./db/db.json')
 
+// HTTP MEthods
+// GET, POST, PUT, PATCH, DELETE, HEAD, OPTIONS
+
+
 
 // function writeToDB(note) {
 //     fs.appendFileSync('./db/db.json', JSON.stringify(note));
 // }
 
-app.get('/', (req, res) =>
+app.get('/', (req, res) => {
   //res.sendFile(path.join(__dirname, './public/index.html'))
+ //  console.log(req);   // USER Submitting form data
   res.sendFile(path.join(__dirname, './public/index.html'))
-);
+ // res.send("Hello There"); // SEND STRING type data
+ // res.json({ "name": "Bull"});
+});
+
+app.post('/', (req, res) => {
+  // create temp object fill in with USER data
+  const newObject = {
+    first_name: req.body.first,
+    last_name: req.body.last
+  }
+
+  // Save this new data into a Database --> Object, Array
+
+
+  // send a response back
+})
+
 
 app.get('/notes', (req, res) => {
 
@@ -49,7 +70,6 @@ app.get('/api/notes', (req, res) => {
 
     res.json(JSON.parse(notesData));
     //console.log(res.json(JSON.parse(notesData)));
-
 });
 
 app.post('/api/notes', (req, res) => {
@@ -58,10 +78,11 @@ app.post('/api/notes', (req, res) => {
     const newNotes = {
         title: req.body.title,
         text: req.body.text,
-        note_id: uuid.v1()
+        note_id: uuid
       };
 
       let notesData = fs.readFileSync('./db/db.json', 'utf8')
+      //let notesData = db;
 
       let notesDataJSON = JSON.parse(notesData);
       notesDataJSON.push(newNotes)
@@ -76,6 +97,20 @@ app.post('/api/notes', (req, res) => {
     );
 
     return res.json(notesDataJSON )
+
+});
+
+app.delete('/api/notes/:id', (req, res) => {
+
+  console.log(req.params.id)
+  // console.log(req.body.name)
+  // we want to capture what the ID is
+
+  // Make a request for our DATA --> fs.readFile()
+
+  // remove specified ID (Find the record by ID)
+
+  // Rewrite the data without the old record (Return(respond) with the Information)
 
 });
 
